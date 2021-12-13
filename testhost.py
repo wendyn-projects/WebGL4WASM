5410c80a665a084046a8d43f4bda694295c70137
0xAA55 <weixiaobao213@qq.com>
2021-12-13 17:41:47 +0800

    Added a script that runs a HTTP server on port 8000, using `testwww` as webroot.

---
+++ testhost.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+import http.server
+import socketserver
+
+PORT = 8000
+DIRECTORY = "testwww"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at port", PORT)
+    httpd.serve_forever()
