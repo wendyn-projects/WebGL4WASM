9021f3a04b1e698170b264929bce208186e722bb
Vaclav [WENDYN] Nemec <wendyncoding@gmail.com>
2023-07-09 01:25:09 +0200

    Makefile does all of this for us _(change &rarr; build)_.

--- compile.py
+++ compile.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
-import subprocess
-import os
-import shutil
+from subprocess import run
+from os import path
 
 srcdir = "testc"
 wwwdir = "testwww"
 filename = "program.wasm"
-srcpath = "%s/%s" % (srcdir, filename)
-wwwpath = "%s/%s" % (wwwdir, filename)
-subprocess.run(["make", "-C", srcpath])
 
-if os.path.exists(srcpath):
-	if os.path.exists(wwwpath): os.remove(wwwpath)
-
-if not os.path.exists(wwwpath):
-	shutil.copyfile(srcpath, wwwpath)
-
+run(["make", path.abspath(path.join(wwwdir, filename)), '-C', srcdir])
